c00482e63ca7f652f9465c845f9234dd1406d236
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-01 15:20:27 -0400

    new dependencies

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const apiRoutes = require('./routes/api');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+const DB_URI = process.env.MONGO_URI;
+
+mongoose.connect(DB_URI, { useUrlParser: true, userUnifiedTopology: true})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
